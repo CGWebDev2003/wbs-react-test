83862c454993edb2c889366df272d861748390c6
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2023-08-28 09:12:10 +0000

    Add Header component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-
-// function tick() {
-//   const element = (
-//     <div>
-//       <h1>Hello World</h1>
-//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//     </div>
-//   );
-//   root.render(element)
-// }
-
-// setInterval(tick, 1000)
+import * as ReactDOM from 'react-dom';
+import './styles.css'
 
 function Welcome(props) {
   return <h1>Hellooooooo, {props.name}</h1>
 }
 
-const welc = <Welcome name="Colin" />
+function Header() {
+  return <div className="header">Header</div>
+}
 
-root.render(welc)
+function App() {
+  return(  
+    <div>
+      <Header />
+
+      <Welcome name="Colin" />
+      <Welcome name="Karl Hainz" />
+      <Welcome name="Kai Uwe" />
+      <Welcome name="Kevin" />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
