752c8376020092ae2dca5b9912a35d5ddb49c7c2
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2023-08-28 09:18:43 +0000

    Add spacer component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import * as ReactDOM from 'react-dom';
 import './styles.css'
 
-function Welcome(props) {
-  return <h1>Hellooooooo, {props.name}</h1>
-}
-
 function Header() {
   return <div className="header">Header</div>
+}
+
+function Spacer() {
+  return <div className="spacer"></div>
+}
+
+function Welcome(props) {
+  return <h1 className="welcome">Hellooooooo, {props.name}</h1>
 }
 
 function App() {
   return(  
     <div>
       <Header />
+      <Spacer height="500" />
+
 
       <Welcome name="Colin" />
       <Welcome name="Karl Hainz" />
