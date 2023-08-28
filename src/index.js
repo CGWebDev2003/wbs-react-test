602b78778a917f4d302690673022c685d64b1d07
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2023-08-28 08:47:54 +0000

    create first component

--- src/index.js
+++ src/index.js
@@ -7,14 +7,22 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello World</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  root.render(element)
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello World</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   root.render(element)
+// }
+
+// setInterval(tick, 1000)
+
+function Welcome(props) {
+  return <h1>Hellooooooo, {props.name}</h1>
 }
 
-setInterval(tick, 1000)
+const welc = <Welcome name="Colin" />
+
+root.render(welc)
